cd469da1a165ef3ec9f771a89f96ad9a4adff440
Ezhik_Manu <iljastan@mail.ru>
2021-10-05 14:22:27 +0300

    another commit

--- src/test/java/com/iljasstan/second_test.java
+++ src/test/java/com/iljasstan/second_test.java
@@ -3,9 +3,12 @@ package com.iljasstan;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SimpleTest {
+public class second_test {
     @Test
-    void simpleTest() {
+
+
+
+    void anotherSimpleTest() {
         Assertions.assertTrue(true);
     }
 }
